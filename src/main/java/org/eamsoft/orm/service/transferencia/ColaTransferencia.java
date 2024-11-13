82785fea6614ab3f31992ef5c0ded5002113d1e2
Juanearias99 <arias.juan.6984@eam.edu.co>
2024-11-13 15:27:16 -0500

    Implementacion de Cola de Prioridad para Cotizantes Aprobados

--- src/main/java/org/eamsoft/orm/service/transferencia/ColaTransferencia.java
+++ src/main/java/org/eamsoft/orm/service/transferencia/ColaTransferencia.java
@@ -1,4 +1,25 @@
 package org.eamsoft.orm.service.transferencia;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+import org.eamsoft.orm.modelo.Cotizante;
 
 public class ColaTransferencia {
+    private PriorityQueue<Cotizante> cola;
+
+    public ColaTransferencia() {
+        this.cola = new PriorityQueue<>(new ComparadorPrioridadCotizante());
+    }
+
+    public void agregarCotizante(Cotizante cotizante) {
+        cola.add(cotizante);
+    }
+
+    public Cotizante obtenerSiguienteCotizante() {
+        return cola.poll();
+    }
+
+    public boolean estaVacia() {
+        return cola.isEmpty();
+    }
 }
+
