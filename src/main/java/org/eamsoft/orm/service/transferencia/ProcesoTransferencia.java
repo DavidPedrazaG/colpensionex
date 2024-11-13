b7fe6c725f5b230d350cb3afa3b09e045b5c852c
KellyVanegas2004 <kellydahianavo2013@gmail.com>
2024-11-13 15:56:28 -0500

    integracion de cola de prioridad en el proceso de transferencia

--- src/main/java/org/eamsoft/orm/service/transferencia/ProcesoTransferencia.java
+++ src/main/java/org/eamsoft/orm/service/transferencia/ProcesoTransferencia.java
@@ -1,4 +1,38 @@
 package org.eamsoft.orm.service.transferencia;
 
+
+import org.eamsoft.orm.modelo.Cotizante;
+import org.eamsoft.orm.service.ValidadorTransferencia;
+import org.eamsoft.orm.service.validation.results.ResultadoValidacion;
+
+import java.util.List;
+
 public class ProcesoTransferencia {
+    private ColaTransferencia colaTransferencia;
+    private ValidadorTransferencia validador;
+
+    public ProcesoTransferencia(ValidadorTransferencia validador) {
+        this.colaTransferencia = new ColaTransferencia();
+        this.validador = validador;
+    }
+
+    public void procesarCotizantes(List<Cotizante> cotizantes) {
+        for (Cotizante cotizante : cotizantes) {
+            ResultadoValidacion resultado = validador.validar(cotizante);
+            if (resultado.esAprobado()) {
+                colaTransferencia.agregarCotizante(cotizante);
+                System.out.println("Cotizante " + cotizante.getNombre() + " aprobado y agregado a la cola.");
+            } else {
+                System.out.println("Cotizante " + cotizante.getNombre() + " rechazado: " + resultado.getMotivo());
+            }
+        }
+    }
+
+    public void despacharCotizantes() {
+        System.out.println("\n--- Despacho de Cotizantes en Orden de Prioridad ---");
+        while (!colaTransferencia.estaVacia()) {
+            Cotizante cotizante = colaTransferencia.obtenerSiguienteCotizante();
+            System.out.println("Procesando cotizante: " + cotizante.getNombre() + " (Documento: " + cotizante.getDocumento() + ")");
+        }
+    }
 }
